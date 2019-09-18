1e5a8e87aadd9f58fdcfb4301d4e0738283a3d5f
Paul Leclerc <paulleclerc@icloud.com>
2019-09-18 18:47:46 +0200

    Added first test

---
+++ RecipleaseTests/RecipleaseTests-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RecipleaseTests-Bridging-Header.h
+//  Reciplease
+//
+//  Created by Paul Leclerc on 18/09/2019.
+//  Copyright © 2019 Paul Leclerc. All rights reserved.
+//
+
+#ifndef RecipleaseTests_Bridging_Header_h
+#define RecipleaseTests_Bridging_Header_h
+
+
+#endif /* RecipleaseTests_Bridging_Header_h */
