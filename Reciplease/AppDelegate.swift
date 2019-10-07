693845f4076e7ef8cda08dba591001c6929e3655
Paul Leclerc <paulleclerc@icloud.com>
2019-10-07 13:51:53 +0200

    Reincluded CoreData

--- Reciplease/AppDelegate.swift
+++ Reciplease/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -52,4 +53,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // also applicationDidEnterBackground:.
     }
 
+// MARK: Core Data
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Reciplease")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    static var persistentContainer: NSPersistentContainer {
+        return (UIApplication.shared.delegate as! AppDelegate).persistentContainer
+    }
+    
+    static var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
 }
